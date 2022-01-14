54f61b536e57dfb6076ed9a633a3eaec6cce5431
Johdasgran <dampig14@gmail.com>
2022-01-14 09:54:42 -0500

    strings

--- src/em6/index.js
+++ src/em6/index.js
@@ -1,14 +1,14 @@
 
-function person(name, age, country) {
-    var name = name || "Johan";
-    var age = age || 22;
-    var country = country || "Colombia";
-    console.log(name, age, country);
-}
+// function person(name, age, country) {
+//     var name = name || "Johan";
+//     var age = age || 22;
+//     var country = country || "Colombia";
+//     console.log(name, age, country);
+// }
 
-function user(name = "Mia", age = 24, country = "CO"){
-    console.log(name, age, country);
-}
+// function user(name = "Mia", age = 24, country = "CO"){
+//     console.log(name, age, country);
+// }
 
 // person();
 // person("Jose", 41, "Costa Rica");
@@ -22,3 +22,9 @@ let world = "Mundo <3";
 console.log(hello + " " + world);
 console.log(`${hello} ${world}`);
 
+let lorem = `lorem ipsu sad asjdjas shdsaldsad sajdoasjd
+sakdlksalñdksañlkdñlasds4
+sadasdsa3
+sdsdmlsam40`
+
+console.log(lorem);
